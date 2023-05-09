87893ada87e68453864bfaf96422bd29aae1acf9
Amal M. Alsattari <91781862+Amal-Mousa@users.noreply.github.com>
2023-05-09 17:58:21 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+import ColorPalette from "./components/ColorPalette";
+import TodoList from "./components/ToDo";
+
+function App() {
+	return (
+		<div className="App">
+      {/* <Counter /> */}
+      {/* <ColorPalette/> */}
+      <TodoList/>
+		</div>
+	);
+}
+
+export default App;
